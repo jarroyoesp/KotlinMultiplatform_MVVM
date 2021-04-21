d8d04d4e01d793c35e1a8661cc8c561b53a34525
Javier Arroyo Espallargas <>
2021-04-21 20:20:33 +0200

    SharedCode Tests - GithubRepository Unit tests

--- SharedCode/src/commonTest/kotlin/com/jarroyo/sharedcode/data/repository/GitHubRepositoryTest.kt
+++ SharedCode/src/commonTest/kotlin/com/jarroyo/sharedcode/data/repository/GitHubRepositoryTest.kt
@@ -1,27 +1,66 @@
 package com.jarroyo.sharedcode.data.repository
 
+import com.jarroyo.sharedcode.base.Response
+import com.jarroyo.sharedcode.base.exception.NetworkConnectionException
+import com.jarroyo.sharedcode.data.source.disk.DiskDataSourceImpl
+import com.jarroyo.sharedcode.data.source.disk.IDiskDataSource
 import com.jarroyo.sharedcode.data.source.network.INetworkDataSource
 import com.jarroyo.sharedcode.domain.usecase.github.getRepos.GetGitHubRepoListRequest
+import com.jarroyo.sharedcode.runTest
+import com.jarroyo.sharedcode.db.User
+import io.mockk.MockKAnnotations
+import io.mockk.impl.annotations.MockK
 import io.mockk.mockk
+import io.mockk.unmockkAll
+import io.mockk.coEvery
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.withContext
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-
 class GitHubRepositoryTest {
+    @MockK
+    lateinit var networkDataSource: INetworkDataSource
+    @MockK
+    lateinit var diskDataSource: IDiskDataSource
 
-    private val mNetworkDataSource = mockk<INetworkDataSource>()
-    private val mGitHubRepository = GitHubRepository(mNetworkDataSource)
+    lateinit var gitHubRepository: GitHubRepository
+
+    @BeforeTest
+    fun setUp() {
+        MockKAnnotations.init(this)
+        gitHubRepository = GitHubRepository(diskDataSource, networkDataSource)
+    }
+
+    @AfterTest
+    fun tearDown() {
+        unmockkAll()
+    }
 
     @Test
-    fun GIVEN_gitHub_username_WHEN_request_repo_list_THEN_return_response() {
+    fun GIVEN_gitHub_username_WHEN_request_repo_list_THEN_return_responseSuccess() = runTest {
+        coEvery { diskDataSource.getUserList() } returns listOf(User(1L, "user"))
+        coEvery { networkDataSource.getGitHubRepoList(any()) } returns Response.Success(emptyList())
         val request = GetGitHubRepoListRequest("GitHubUserName")
 
-        // This actually doesn't work, but makes this test compile. Testing suspending functions is currently
-        // not possible in common modules
-        suspend {
-            mGitHubRepository.getRepos(request)
-            mNetworkDataSource.getGitHubRepoList(request.userName)
-            assertEquals(0, 0)
-        }
+        val response = gitHubRepository.getRepos(request)
+
+        assert(response is Response.Success)
+
+    }
+
+    @Test
+    fun GIVEN_gitHub_username_WHEN_request_repo_list_THEN_return_responseError() = runTest {
+        coEvery { diskDataSource.getUserList() } returns listOf(User(1L, "user"))
+        coEvery { networkDataSource.getGitHubRepoList(any()) } returns Response.Error(NetworkConnectionException())
+        val request = GetGitHubRepoListRequest("GitHubUserName")
+
+        val response = gitHubRepository.getRepos(request)
+
+        assert(response is Response.Error)
+
     }
 }
